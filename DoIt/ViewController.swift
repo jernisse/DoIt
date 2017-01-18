d70478af30a0682bee3b54fe8d085125ce9e560b
Josh Ernisse <jernisse@icloud.com>
2017-01-18 08:41:11 -0600

    Initial Setup of Rows and Cells, Connecting Story Board to ViewController

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        // This is saying look at the above class ViewController to get this information
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        // How Many Rows Are There Going To Be
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello"
+        return cell
+        // What is going to go in each cell, for example Hello will repeat 10 times in this instance
+    }
 }
 
